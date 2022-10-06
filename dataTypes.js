89a5390301d2ac24e1efc91cbada775b558a6930
Rdhwnzaki <zaki85.rmz@gmail.com>
2022-10-06 16:17:08 +0700

    cobacoba

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+//Number
+var nomor = 24;
+//String
+var huruf = 'Ridhwan';
+//Boolean
+var boolean = true;
+//Function
+var fungsi = function(){
+    console.log('Ini function')
+} 
+//Object
+var objectData = {
+    nama : 'Ridhwan',
+    umur : 19
+};
+//Null
+var kosong = null;
+//Undefined
+var data;
+console.log(data)
+//TypeOff
+console.log(typeof(nomor))
